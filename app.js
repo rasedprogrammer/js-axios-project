193549f5a8a3805330715e1171bf09d06cb056fe
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-08 23:51:23 +0600

    adding prettierrc and netlify

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const getDadJoks = async () => {
-  const res = await axios.get("https://icanhazdadjoke.com/");
+  const config = { header: { Accept: "application.json" } };
+  const res = await axios.get("https://icanhazdadjoke.com/", config);
   console.log(res);
 };
